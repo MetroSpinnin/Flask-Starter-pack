121dc24f1afeacf2b9149edb0764ec1eb06e95a3
Jim <46245794+MetroSpinnin@users.noreply.github.com>
2019-03-29 08:12:23 +0100

    Setting up the dependency files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#Please note that the Depedencies for running this file would be included her
+# for now the Scripts folder have not been added and therefore will throw an 
+# error if run.. when the script folder is gotten the app can be run by the
+# command: Python app.py (serves as main app loop)
+
+from app import app
+from db_config import mysql
+from main import appLoop
+
+#please am still editing this file.. kindly stay out till it done
